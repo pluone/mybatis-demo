1631e9c48da7cb8c7017e580af1083f068e5cefd
Frank Pei <pluone@outlook.com>
2021-03-05 10:42:55 +0800

    finish MBG and Example usage

--- src/main/java/org/example/simple/mapper/CountryMapper.java
+++ src/main/java/org/example/simple/mapper/CountryMapper.java
@@ -1,9 +1,32 @@
 package org.example.simple.mapper;
 
-import org.example.simple.model.Country;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.example.simple.model.Country;
+import org.example.simple.model.CountryExample;
 
 public interface CountryMapper {
+    int countByExample(CountryExample example);
+
+    int deleteByExample(CountryExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Country record);
+
+    int insertSelective(Country record);
+
+    List<Country> selectByExample(CountryExample example);
+
+    Country selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Country record, @Param("example") CountryExample example);
+
+    int updateByExample(@Param("record") Country record, @Param("example") CountryExample example);
+
+    int updateByPrimaryKeySelective(Country record);
+
+    int updateByPrimaryKey(Country record);
+
     List<Country> selectAll();
 }
